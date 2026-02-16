de13efb5fd130ee22f4700f2c2597887e9c3f7ae
Appas <appasm321@gmail.com>
2026-02-16 20:03:49 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,11 +1,9 @@
-# db_config.py
 import os
 
 db_config = {
-    "host": os.getenv("DB_HOST", "mysql-3f297111-appasm321-e8a6.k.aivencloud.com"),
-    "user": os.getenv("DB_USER", "avnadmin"),
-    "password": os.getenv("DB_PASSWORD", "AVNS_FLKQh4Rgnsk6jfp1fxG"),
-    "database": os.getenv("DB_NAME", "defaultdb"),
-    "port": int(os.getenv("DB_PORT", 15298)),
-    "ssl_ca": "/path/to/ca.pem",  # Aiven provides a CA certificate file
+    "host": os.getenv("MYSQLHOST"),
+    "user": os.getenv("MYSQLUSER"),
+    "password": os.getenv("MYSQLPASSWORD"),
+    "database": os.getenv("MYSQLDATABASE"),
+    "port": int(os.getenv("MYSQLPORT")),
 }
