28a270ead46a44d5c90b987c9e3bbded4b7ddb1e
Appas <appasm321@gmail.com>
2026-02-16 13:16:10 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,17 +1,12 @@
 import os
-from dotenv import load_dotenv
-
-# Load .env variables
-load_dotenv()
 
 db_config = {
-    'host': os.getenv("DB_HOST"),
-    'port': int(os.getenv("DB_PORT")),
-    'user': os.getenv("DB_USER"),
-    'password': os.getenv("DB_PASS"),
-    'database': os.getenv("DB_NAME")
+    "host": os.getenv("DB_HOST", "mysql.railway.internal"),
+    "port": int(os.getenv("DB_PORT", 3306)),
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASSWORD", "lYbgLpYGrcWPUsVWRwPnGswYFsieywmC"),
+    "database": os.getenv("DB_NAME", "railway")
 }
 
-# Debug: check loaded config
+# Optional debug
 print("DB Config Loaded:", db_config)
-
